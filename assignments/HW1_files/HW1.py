40d3adeed181933222848b579811527e48c56d3b
Aaron Mau <aaronmau40@gmail.com>
2025-01-14 10:26:06 -0800

    Init HW1

---
+++ assignments/HW1_files/HW1.py
@@ -0,0 +1,81 @@
+"""Homework 1 for CSE-41273"""
+# Your name goes here, but keep the # and a space.
+# ^^^^^^ Lines beginning with a "# " are comments.
+# So, MY homework would have LINE 2 as:
+# Diane Chen
+#
+# Please use the name you are registered under; not nicknames or alternates.
+# Be sure to read the instructions that came with this file.
+# If you do not follow instructions, you will not be successful.
+# PLEASE ALWAYS PUT YOUR NAME ON LINE 2. DO NOT CHANGE MY ARRANGEMENT OF LINES.
+# MY AUTOMATED TESTING DEPENDS UPON THIS.
+#
+# NOTE: The only code that should be here when you submit it are the functions.
+# NOTE: There should be NO print statements anywhere when you submit the file.
+
+
+# Function 1
+def silly_case(sentence):
+    """Given a sentence, convert it to a string such that each word starts with
+        a lower case letter, and the remaining letters of the word are
+        in upper case.
+        Return the silly case string.
+    """
+    # Put your code here
+
+
+# Function 2
+def dash_stringify(list_of_words):
+    """Given a list of word strings, return a single string with all the word
+        strings together, with ' - ' in between the words.
+    """
+    # Put your code here
+
+
+# Function 3
+def is_perfect_square(any_number):
+    """Given any number, return True if it is a perfect square,
+        else return False
+    """
+    # Put your code here
+
+
+# Function 4
+def comfort_level(temperature):
+    """Given a temperature in Fahrenheit, return a string that is a
+        comment on the comfort level.
+    """
+    # Put your code here
+
+
+# Function 5
+def fstring_practice(numbers, divisor):
+    """Given a list of numbers and a divisor, return a list of strings:
+        number divided by divisor is result
+        for each number in the input list of numbers.
+        You must use f-strings!
+        See instructions for more details.
+    """
+    # Put your code here
+
+
+# Function 6
+def string_info(sentences):
+    """Given a string (multi-line probably), return a dictionary such that:
+        Each key is a line number of a line of the string, starting at 1.
+        The corresponding value is a tuple containing 3 items:
+            the line,
+            number of characters in the line,
+            number of words in the line
+        See the instructions for more details.
+    """
+    # Put your code here
+
+
+# Function 7
+def get_earliest(date1, date2):
+    """Given 2 date strings in "MM/DD/YYYY" format, return earliest one.
+        DO NOT use the datetime module!
+        Use string manipulation and unpacking.
+    """
+    # Put your code here
