7418f0d9c9e35d2229b8c86b2e1a1037f0c1fc1c
Aaron Mau <aaronmau40@gmail.com>
2025-01-29 11:33:10 -0800

    Init HW3

---
+++ assignments/HW3_files/HW3.py
@@ -0,0 +1,23 @@
+""" Homework assignment for Week 3 """
+# Put your name here
+
+
+def len_safe(object):
+    """Return the length of object or -1 if the object has no length.
+       So no matter what object it is given, an error should not
+       be raised to the user.
+       The returned value -1 is a number, NOT a string.
+    """
+
+
+def list_pairs(in_list):
+    """Take a list as input and return a list of pairs (2-tuples). Each pair
+        contains an item from the list and the following item.
+        See the instructions for details.
+    """
+
+
+def unique(sequence):
+    """Yield elements of sequence in order, skipping any duplicate values.
+        See the instructions for details.
+    """
---
+++ assignments/HW3_files/HW3_cli.py
@@ -0,0 +1,13 @@
+""" CLI program to for HW3 homework """
+# Put your name here
+import argparse
+
+
+if __name__ == "__main__":
+    # Set up argparse parser. You have more to do here...
+    parser = argparse.ArgumentParser()
+    # Add the arguments here with parser.add_argument
+
+    args = parser.parse_args()
+    # Based on the argument information returned in the args variable,
+    # execute whatever the flags say to do here, in the correct order.
